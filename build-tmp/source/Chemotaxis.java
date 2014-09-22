1415ab725a718d2aa79cd0b04d8fb8e21c0e7150
unknown <naguan@697-334-Forlond.sf.k12.internal>
2014-09-22 11:09:11 -0700

    Chemotaxis.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 
 Bacteria[] virus, germ;
-void setup()   
+public void setup()   
 {    
   size(500, 500);
   background(255);
@@ -14,7 +30,7 @@ void setup()
   //frameRate(5);
   //initialize bacteria variables here
 }   
-void draw()   
+public void draw()   
 {    
   background(255);
  for(int i = 0; i < virus.length; i++)
@@ -38,19 +54,28 @@ class Bacteria
     colors2 = (int)(Math.random()*256);
     colors3 = (int)(Math.random()*256);
   }
-  void move()
+  public void move()
   {
     myX = myX + (int)(Math.random()*7)-3;
     myY = myY + (int)(Math.random()*7)-3;
   }
-  void show()
+  public void show()
   {
     fill(colors1, colors2, colors3);
     ellipse(myX, myY, 10, 10);
   }
-  void jump()
+  public void jump()
   {
   	 myX = (int)(Math.random()*501);
     myY = (int)(Math.random()*501);
   }
 }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
